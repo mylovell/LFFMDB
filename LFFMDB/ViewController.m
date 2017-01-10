ec5433a5f1376f0f14e72fcccaf5ecc4e18e0ac4
mylovell <906197961@qq.com>
2017-01-11 07:28:28 +0800

    集成fmdb
    用cocoapods集成fmdb第三方

---
+++ LFFMDB/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  LFFMDB
+//
+//  Created by LuoFeng on 2017/1/11.
+//  Copyright © 2017年 LuoFengcompany. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <FMDB.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Podfile
@@ -0,0 +1,10 @@
+
+platform :ios, '8.0'
+def pods
+    pod 'FMDB'
+
+end
+
+target 'LFFMDB' do
+    pods
+end
